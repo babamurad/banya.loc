d04ff74e0575dba5f0af7e95e7c9b726ffcbaca9
babamurad <babamurad2010@yandex.ru>
2024-06-19 06:39:35 +0500

    Auth User from LockScreen

--- app/Livewire/LockScreenComponent.php
+++ app/Livewire/LockScreenComponent.php
@@ -2,7 +2,9 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+
 
 class LockScreenComponent extends Component
 {
@@ -16,5 +18,24 @@ class LockScreenComponent extends Component
     {
         auth()->logout();
         return redirect()->route('login');
+    }
+
+    public function login()
+    {
+        if (Auth::attempt($user))
+        {
+            Auth::user($user);
+            //dd(Auth::check());
+            if (auth()->user()->utype == 'KSS' || auth()->user()->utype == 'ADM'){
+                //dd(auth()->user()->utype.' - '.auth()->user()->status);
+                return redirect()->route('admin');
+            } else {
+                //dd(auth()->user()->utype.' - '.auth()->user()->status);
+                $this->redirectRoute('lock-screen');
+            }
+            session()->flash('success', __('Вы авторизованы.'));
+        } else {
+            session()->flash('error', __('Имя или пароль неверны'));
+        }
     }
 }
